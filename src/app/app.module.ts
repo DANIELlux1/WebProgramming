2ea0eeda081f75d3e3a4cff3ca22c292fa08f5b7
DANIELlux1 <daniellux1moreira@gmail.com>
2020-01-01 12:35:25 +0100

    init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
 
 import { AppComponent } from './app.component';
+import { AuthComponent } from './auth/auth.component';
+import { UserComponent } from './user/user.component';
+import { HeaderComponent } from './header/header.component';
+import { InternshipComponent } from './internship/internship.component';
+import { AdminComponent } from './admin/admin.component';
+import { TweetComponent } from './tweet/tweet.component';
+import { HistoryComponent } from './history/history.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    UserComponent,
+    HeaderComponent,
+    InternshipComponent,
+    AdminComponent,
+    TweetComponent,
+    HistoryComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/internship/internship.model.ts
@@ -0,0 +1,4 @@
+export class Internship{
+
+    constructor(){}
+}
